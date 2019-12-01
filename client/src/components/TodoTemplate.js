39c0e34580b7fcc21657551527800bdc67b70221
soohongbin <shzzang97@naver.com>
2019-12-01 18:47:11 +0900

    Add Node.js Express Server Module

---
+++ client/src/components/TodoTemplate.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './TodoTemplate.css';
+
+const TodoTemplate = ({form, palette, children}) => {
+  return (
+    <main className="todo-list-template">
+      <div className="title">
+        오늘의 해야할 일
+      </div>
+      <section className="palette-wrapper">
+        {palette}
+      </section>
+      <section className="form-wrapper">
+        {form}
+      </section>
+      <section className="todos-wrapper">
+        { children }
+      </section>
+    </main>
+  );
+};
+
+export default TodoTemplate;
